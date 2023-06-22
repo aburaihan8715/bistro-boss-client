a04e696236dc8d989d17e89312382cde77f244e9
aburahan8721 <aburaihan8721@gmail.com>
2023-06-22 21:46:13 +0600

    jwt implement done

--- src/hooks/useFetchCartData.js
+++ src/hooks/useFetchCartData.js
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useFetchCartData = () => {
   const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
   const { data: carts = [], refetch } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
-      const data = await fetch(`http://localhost:5001/carts?email=${user?.email}`);
-      return data.json();
+      const data = await axiosSecure(`/carts?email=${user?.email}`);
+      return data.data;
     },
   });
 
